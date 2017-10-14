a10447505109c3c82522b1b5f548eaafec9cd86d
Yael <yaelgersh92@gmail.com>
2017-10-14 20:47:44 +0300

    bad code! not working on yael's computer can't connect to firebase

--- Park-Bark/AppDelegate.swift
+++ Park-Bark/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import GoogleSignIn
+//import FBSDKCoreKit
 import Firebase
 
 @UIApplicationMain
@@ -17,13 +19,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        //FirebaseApp.configure()
-        
+        FirebaseApp.configure()
         return true
+        //FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        //GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
+        //return true
     }
-
+    
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+//        let handle = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+//        
+//        return handle
+//    }
+    
+    
+//    public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+//        return FBSDKApplicationDelegate.sharedInstance().application(
+//            application,
+//            open: url as URL!,
+//            sourceApplication: sourceApplication,
+//            annotation: annotation)
+//    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ Park-Bark/ProfileViewController.swift
@@ -0,0 +1,176 @@
+//
+//  ProfileViewController.swift
+//  Park-Bark
+//
+//  Created by Yael on 12/10/2017.
+//  Copyright © 2017 park-bark. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var dogButton: UIButton!
+    @IBOutlet weak var dogPic: UIImageView!
+    
+    @IBOutlet weak var datePickerText: UITextField!
+    let datePicker = UIDatePicker()
+
+    @IBOutlet weak var genderCircle: UIImageView!
+    let genderPic = ["bluecircle", "pinkcircle"]
+    
+    
+    @IBOutlet weak var genderSegmentControll: UISegmentedControl!
+   
+    @IBOutlet weak var bigDogPic: UIImageView!
+    @IBOutlet weak var mediumBigDogPic: UIImageView!
+    @IBOutlet weak var mediumSmallDogPic: UIImageView!
+    @IBOutlet weak var smallDogPic: UIImageView!
+    let dogSize = ["bigDog", "mediumBig", "mediumSmall", "small"]
+    let GREEN = "Green"
+    let BLACK = "Black"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createDatePicker()
+        
+        //dogPic.layer.borderWidth = 1
+        //dogPic.layer.masksToBounds = false
+        //dogPic.layer.borderColor = UIColor.black.cgColor
+        dogPic.layer.cornerRadius = dogPic.frame.height/2
+        dogPic.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func createDatePicker(){
+        datePicker.datePickerMode = .date
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneButton], animated: false)
+        
+        datePickerText.inputAccessoryView = toolbar
+        datePickerText.inputView = datePicker
+        
+    }
+    
+    func donePressed(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        
+        datePickerText.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func pickADog(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            else{
+                // cant run in debug mode
+                print("Camera not available")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            dogPic.image = image
+            dogPic.alpha = 1
+            
+            dogButton.setTitle("", for: .normal)
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func genderChanged(_ sender: Any) {
+        genderCircle.image = UIImage(named:genderPic[genderSegmentControll.selectedSegmentIndex])
+    }
+
+    
+    
+    @IBAction func pickedBig(_ sender: Any) {
+        bigDogPic.image = UIImage(named: dogSize[0]+GREEN)
+        mediumBigDogPic.image = UIImage(named: dogSize[1]+BLACK)
+        mediumSmallDogPic.image = UIImage(named: dogSize[2]+BLACK)
+        smallDogPic.image = UIImage(named: dogSize[3]+BLACK)
+    }
+
+    @IBAction func pickedMediumBig(_ sender: Any) {
+        bigDogPic.image = UIImage(named: dogSize[0]+BLACK)
+        mediumBigDogPic.image = UIImage(named: dogSize[1]+GREEN)
+        mediumSmallDogPic.image = UIImage(named: dogSize[2]+BLACK)
+        smallDogPic.image = UIImage(named: dogSize[3]+BLACK)
+    }
+    
+    @IBAction func pickedMadiumSmall(_ sender: Any) {
+        bigDogPic.image = UIImage(named: dogSize[0]+BLACK)
+        mediumBigDogPic.image = UIImage(named: dogSize[1]+BLACK)
+        mediumSmallDogPic.image = UIImage(named: dogSize[2]+GREEN)
+        smallDogPic.image = UIImage(named: dogSize[3]+BLACK)
+    }
+    
+    @IBAction func pickedSmall(_ sender: Any) {
+        bigDogPic.image = UIImage(named: dogSize[0]+BLACK)
+        mediumBigDogPic.image = UIImage(named: dogSize[1]+BLACK)
+        mediumSmallDogPic.image = UIImage(named: dogSize[2]+BLACK)
+        smallDogPic.image = UIImage(named: dogSize[3]+GREEN)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
