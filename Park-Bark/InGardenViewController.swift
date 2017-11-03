d6a83252a2bcc60ab45ee034dde66ad187f67ac1
Yael <yaelgersh92@gmail.com>
2017-11-03 15:42:45 +0200

    GUI update - fixed garden table

--- Park-Bark/InGardenViewController.swift
+++ Park-Bark/InGardenViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class InGardenViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UpdateInGardenDelegate {
     
+    @IBOutlet weak var dogsInGardenTable: UITableView!
+    var dogsInGardenList = FBDatabaseManagment.getInstance().getDogsInGardenList()
+    let sizes: [String] = ["קטן", "בינוני", "גדול", "גדול מאוד"]
+    
     func dbUpdated() {
         DispatchQueue.global(qos: .background).async {
             self.dogsInGardenList = FBDatabaseManagment.getInstance().getDogsInGardenList()
@@ -18,19 +22,12 @@ class InGardenViewController: UIViewController, UITableViewDataSource, UITableVi
                 self.dogsInGardenTable.reloadData()
             }
         }
-        
     }
 
-    var dogsInGardenList = FBDatabaseManagment.getInstance().getDogsInGardenList()
-    
-    @IBOutlet weak var dogsInGardenTable: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         FBDatabaseManagment.getInstance().updateInGardenDelegate = self
-        
-        
-        
+    
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: " ", style: .plain, target: nil, action: nil)
 
         // Do any additional setup after loading the view.
@@ -51,42 +48,51 @@ class InGardenViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dogCell", for: indexPath) as! InGardenTableViewCell
-        cell.dogId = dogsInGardenList[indexPath.row].id
         
-        if let dog = UserApp.getInstance().findDogById(id: cell.dogId){
-        
-            if (dog.urlImage) != nil{
-                //let url = URL(fileURLWithPath: UserApp.getInstance().dogs[id].urlImage!)
-                let url = URL(string: dog.urlImage!)
-                let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-                    if error != nil{
-                        print(error!)
-                        return
-                    }
-                    DispatchQueue.main.async {
-                        cell.dogImage.image = UIImage(data: data!)
-                        
-                    }
-                })
-                task.resume()
-            }
-        }
         cell.dogImage.layer.cornerRadius = cell.dogImage.frame.height/2
         cell.dogImage.clipsToBounds = true
         
         
+        let dog: Dog = dogsInGardenList[indexPath.row]
+        cell.dogId = dog.id
+        cell.nameLabel.text = " שם: \(dog.name!)"
         
-        
-        
-        cell.nameLabel.text = dogsInGardenList[indexPath.row].name
-        cell.ageLabel.text = "\(dogsInGardenList[indexPath.row].day!)/\(dogsInGardenList[indexPath.row].mounth!)/\(dogsInGardenList[indexPath.row].year!)"
-        cell.sizeLabel.text = dogsInGardenList[indexPath.row].race
-        if(UserApp.getInstance().following.contains(dogsInGardenList[indexPath.row].id!)){
-            cell.likeButton.setImage(UIImage(named: "heartfull"), for: .normal)
+        if (dog.urlImage) != nil{
+            let url = URL(string: dog.urlImage!)
+            let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    return
+                }
+                DispatchQueue.main.async {
+                    cell.dogImage.image = UIImage(data: data!)
+                    
+                }
+            })
+            task.resume()
         }
         
+        var tempDate = DateComponents()
+        tempDate.year = dog.year
+        tempDate.month = dog.mounth
+        tempDate.day = dog.day
         
+        let now = Date()
+        let calendar = Calendar.current
+        let dogBirthday = calendar.date(from: tempDate)
         
+        let yearComponents = calendar.dateComponents([.year], from: dogBirthday!, to: now)
+        let monthComponents = calendar.dateComponents([.month], from: dogBirthday!, to: now)
+        let months = Int((Double(Int(monthComponents.month!) % 12)/12)*10)
+        let theAge = "\(yearComponents.year!).\(String(months))"
+        
+        cell.ageLabel.text = " גיל:  \(theAge)"
+        let size: String = sizes[dog.size]
+        
+        cell.sizeLabel.text = " \(dog.race!), \(size)"
+        if(UserApp.getInstance().following.contains(dog.id!)){
+            cell.likeButton.setImage(UIImage(named: "heartfull"), for: .normal)
+        }
         
         return cell
     }
@@ -95,13 +101,13 @@ class InGardenViewController: UIViewController, UITableViewDataSource, UITableVi
         print("\(self) InGarden - dead")
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title:"\(dogsInGardenList[indexPath.row].name!)", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "חזרה", style: .default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        dogsInGardenTable.cellForRow(at: IndexPath.row).
+        self.present(alert, animated: true, completion: nil)
+    }*/
+    
 }
